ddc43fc4b46b8e26264adbb66c4bb0b7f602e57a
Wittypotato <18651218857@163.com>
2019-07-22 01:32:50 +0800

    nav

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import 'font-awesome/css/font-awesome.min.css'
-import './index.css'
-import './index2.scss'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Layout from 'component/layout/index.jsx'
+import Home from 'page/home/index.jsx'
 
+class App extends React.Component{
+    render() {
+        return (
+            <BrowserRouter>
+                <Layout>
+                    <Switch>
+                        <Route exact path="/" component={Home}></Route>
+                        <Route path="/product" component={Home}></Route>
+                        <Route path="/product-category" component={Home}></Route>
+                    </Switch>
+                </Layout>
+            </BrowserRouter>
+        )
+    }
+}
 ReactDom.render(
-    <div>
-        <i className="fa fa-address-book"></i>
-        <h1>Hello World</h1>
-    </div>,
+    <App />,
     document.getElementById('root')
 )
